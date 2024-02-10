a77d2e68ecbe3a1018a8cb38da1228660e84e8f8
Volodymyr Kuzmin <vlkzmn@gmail.com>
2024-02-10 23:48:12 +0200

    fix

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,27 @@ import './HomePage.scss';
 export const HomePage = () => {
   return (
     <div className="home-page">
-      Home Page
+      <div className="home-page__slider">
+        <img
+          src="img/slider-6.jpg"
+          className="home-page__slider-image"
+          alt="slider"
+        />
+
+        <div className="home-page__slider-text">
+          <p className="home-page__slider-sub-text">
+            З думкою про інших
+          </p>
+
+          <p className="home-page__slider-text-main">
+            Простягни руку допомоги тим
+            <br />
+            Хто її потребує
+          </p>
+        </div>
+
+        <div className="home-page__slider-map" />
+      </div>
     </div>
   );
 };
