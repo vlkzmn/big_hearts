8413e8db477a04510f51f453764d81d794b085bb
Volodymyr Kuzmin <vlkzmn@gmail.com>
2024-02-16 00:02:00 +0200

    add arrow up

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,51 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
+import { useEffect, useState } from 'react';
+import cn from 'classnames';
+
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 import './App.scss';
 
-export const App = () => (
-  <div className="App">
-    <Header />
+export const App = () => {
+  const [isVisible, setIsVisible] = useState(false);
 
-    <main className="App__main">
-      <Outlet />
-    </main>
+  useEffect(() => {
+    const toggleVisibility = () => {
+      if (window.pageYOffset > 300) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
 
-    <Footer />
-  </div>
-);
+    window.addEventListener('scroll', toggleVisibility);
+
+    return () => window.removeEventListener('scroll', toggleVisibility);
+  }, []);
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  return (
+    <div className="app">
+      <Header />
+
+      <main className="app__main">
+        <Outlet />
+      </main>
+
+      <Footer />
+
+      <button
+        type="button"
+        className={cn('app__arrow-up', { 'app__arrow-up--active': isVisible })}
+        aria-label="arrow up button"
+        onClick={() => scrollToTop()}
+      />
+    </div>
+  );
+};
