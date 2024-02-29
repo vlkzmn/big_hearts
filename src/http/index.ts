8f06f37346c27256df74e55a0f2f442da0a5bb24
Volodymyr Kuzmin <vlkzmn@gmail.com>
2024-02-29 18:49:28 +0200

    http client fixed

--- src/http/index.ts
+++ src/http/index.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export function createClient() {
   return axios.create({
     baseURL: 'https://bighearts.onrender.com',
-    withCredentials: true,
+    // withCredentials: true,
   });
 }
